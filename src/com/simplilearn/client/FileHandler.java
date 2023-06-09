02549a4575e2ab87f814fd3b408db781251bfe0c
Hanvesh <hanvesh.pinapothu@mpokket.com>
2023-06-09 10:49:59 +0530

    Adding File Handler Project

---
+++ src/com/simplilearn/client/FileHandler.java
@@ -0,0 +1,10 @@
+package com.simplilearn.client;
+
+import com.simplilearn.services.FileHandlingService;
+
+public class FileHandler {
+    public static void main(String[] args) {
+        FileHandlingService fileManagerService = new FileHandlingService();
+        fileManagerService.startTheApp();
+    }
+}
